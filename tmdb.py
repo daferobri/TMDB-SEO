37f5dbc1dcc7b7824a2f9a91d54fd78814909a9c
David Rodriguez <david.rodriguez@yale.edu>
2025-06-26 21:04:47 +0000

    Fixed dataframe and database creation

--- tmdb.py
+++ tmdb.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import sqlalchemy as db
+import json
 from tmdbv3api import Account, TMDb, Movie
 
 tmdb = TMDb()
@@ -14,13 +15,19 @@ print('You are logged in as', details.username)
 movie = Movie()
 
 query = input('Movie to add to watchlist: ')
-s = movie.search(query)
-s_df = pd.DataFrame.from_dict(s)
+
+s = movie.search(query)._json.get('results', [])
+for result in s:
+	for key, value in result.items():
+		if isinstance(value, (list, dict)):
+			result[key] = json.dumps(result[key])
+
+s_df = pd.DataFrame(s)
 
 engine = db.create_engine('sqlite:///tmdb.db')
 
 s_df.to_sql('movies', con=engine, if_exists='replace', index=False)
 
 with engine.connect() as connection:
-   query_result = connection.execute(db.text("SELECT * FROM movies;")).fetchall()
-   print(pd.DataFrame(query_result))
+  query_result = connection.execute(db.text("SELECT * FROM movies;")).fetchall()
+  print(pd.DataFrame(query_result))
